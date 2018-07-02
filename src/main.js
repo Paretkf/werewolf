8ea58a52d95e7273e6c3a080af53a416e0a7dd5d
Paretkf <paretk7@gmail.com>
2018-07-02 14:54:38 +0700

    add vue-perfect-scrollbar vue-sweetalert2 buefy begeta

--- src/main.js
+++ src/main.js
@@ -3,13 +3,25 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+import 'begeta/css/begeta.min.css'
+import VuePerfectScrollbar from 'vue-perfect-scrollbar'
+import VueSweetalert2 from 'vue-sweetalert2'
 
 Vue.config.productionTip = false
+
+Vue.component('VuePerfectScrollbar', VuePerfectScrollbar)
+
+Vue.use(Buefy)
+Vue.use(VueSweetalert2)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
